ccb4117da610886febe808e734d6976b7b08cd1d
luozhy88 <94740863+luozhy88@users.noreply.github.com>
2023-06-20 09:32:41 +0800

    Update function_STAMPv2.R

--- function_STAMPv2.R
+++ function_STAMPv2.R
@@ -65,84 +65,84 @@ pathway_pdf=function(meta_info=meta_info,pathway_df=pathway_df,meta_col=meta_col
     write.csv(diff,glue::glue("output/", out.name, comparison, "_wilcox.csv"))
     #    diff.sig <- subset(diff, p.value < 0.01)
     diff.sig <- subset(diff, p.adj <= padj)
-    diff.sig$col <- ifelse(diff.sig$meandiff > 0, "01plus", "02minus")
-    
-    
-    
-    DF.tss2 <- reshape2::melt(  DF.tss[,  c("Group", DF.tss_features_col) ]  )
-    #  DF.tss2$Group <- factor(DF.tss2$Group, levels = c("young", str_replace(comparison, pattern = "young-vs-", replacement = "")))
-    DF.tss2$variable <- as.character(DF.tss2$variable)
-    ##
-    DF.tss3 <- DF.tss2[DF.tss2$variable %in% diff.sig$var, ]
-    
-    ## base plot
-    p1 <- ggplot(data = DF.tss3, aes(x = variable, y = value, fill = Group))
-    
-    ### add alternated background
-    for (i in 1:(nrow(diff.sig))) {
-      p1 <- p1 + annotate("rect",
-                          xmin = i - 0.5, xmax = i + 0.5, ymin = 0, ymax = Inf,
-                          fill = ifelse(i %% 2 == 0, "white", "gray95")
-      ) + scale_x_discrete()
+    if(nrow(diff.sig)!=0){
+      
+      diff.sig$col <- ifelse(diff.sig$meandiff > 0, "01plus", "02minus")
+      
+      DF.tss2 <- reshape2::melt(  DF.tss[,  c("Group", DF.tss_features_col) ]  )
+      #  DF.tss2$Group <- factor(DF.tss2$Group, levels = c("young", str_replace(comparison, pattern = "young-vs-", replacement = "")))
+      DF.tss2$variable <- as.character(DF.tss2$variable)
+      ##
+      DF.tss3 <- DF.tss2[DF.tss2$variable %in% diff.sig$var, ]
+      
+      ## base plot
+      p1 <- ggplot(data = DF.tss3, aes(x = variable, y = value, fill = Group))
+      
+      ### add alternated background
+      for (i in 1:(nrow(diff.sig))) {
+        p1 <- p1 + annotate("rect",
+                            xmin = i - 0.5, xmax = i + 0.5, ymin = 0, ymax = Inf,
+                            fill = ifelse(i %% 2 == 0, "white", "gray95")
+        ) + scale_x_discrete()
+      }
+      
+      p1 <- p1 +
+        geom_bar(color = "black", position = "dodge", stat = "summary", fun = "mean") +
+        xlab("") +
+        ylab("Proportion (%)") +
+        theme_minimal() +
+        theme(
+          axis.text.x = element_text(color = "black"),
+          axis.ticks = element_line(color = "black", linewidth = 0.5)
+        ) +
+        coord_flip() +
+        theme(legend.position = "top")
+      # Use custom colors
+      p1 <- p1 + scale_fill_manual(values = c("darkorange", "deepskyblue"))
+      
+      
+      ### base plot
+      p2 <- ggplot(diff.sig, aes(x = var, y = meandiff, fill = col))
+      ### add alternated background
+      for (i in 1:(nrow(diff.sig))) {
+        p2 <- p2 + annotate("rect",
+                            xmin = i - 0.5, xmax = i + 0.5, ymin = -Inf, ymax = Inf,
+                            fill = ifelse(i %% 2 == 0, "white", "gray95")
+        ) + scale_x_discrete()
+      }
+      ## add the other components
+      p2 <- p2 +
+        geom_point(stat = "identity", size = 3, aes(col = col), position = position_dodge(width = 1)) +
+        geom_errorbar(aes(ymin = lwr.ci, ymax = upr.ci),
+                      width = .2, size = 0.5,
+                      position = "dodge"
+        ) +
+        xlab("") +
+        ylab("Difference between proportions (%)") +
+        geom_hline(yintercept = 0, lty = 2) +
+        theme_minimal() +
+        theme(
+          axis.text.x = element_text(color = "black"),
+          axis.ticks.x = element_line(color = "black", linewidth = 0.5)
+        ) +
+        coord_flip() +
+        theme(legend.position = "none") +
+        theme(axis.text.y = element_blank())
+      p2 <- p2 + scale_color_manual(values = c("darkorange", "deepskyblue"))
+      
+      p1 + p2
+      
+      ###
+      ##
+      dir.create("output",recursive = T)
+      # height=5.5
+      ggsave(filename = glue::glue("output/", out.name, comparison,padj, "_metagenomics_smallSize.pdf"), width = 12, height = height)
+      ggsave(filename = glue::glue("output/", out.name, comparison,padj, "_metagenomics_mediumSize.pdf"), width = 12.5, height = height+2)
+      ggsave(filename = glue::glue("output/", out.name, comparison,padj, "_metagenomics_bigSize.pdf"), width = 12.5, height = height+3)
+      
+      #  }
     }
-    
-    p1 <- p1 +
-      geom_bar(color = "black", position = "dodge", stat = "summary", fun = "mean") +
-      xlab("") +
-      ylab("Proportion (%)") +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(color = "black"),
-        axis.ticks = element_line(color = "black", linewidth = 0.5)
-      ) +
-      coord_flip() +
-      theme(legend.position = "top")
-    # Use custom colors
-    p1 <- p1 + scale_fill_manual(values = c("darkorange", "deepskyblue"))
-    
-    
-    ### base plot
-    p2 <- ggplot(diff.sig, aes(x = var, y = meandiff, fill = col))
-    ### add alternated background
-    for (i in 1:(nrow(diff.sig))) {
-      p2 <- p2 + annotate("rect",
-                          xmin = i - 0.5, xmax = i + 0.5, ymin = -Inf, ymax = Inf,
-                          fill = ifelse(i %% 2 == 0, "white", "gray95")
-      ) + scale_x_discrete()
-    }
-    ## add the other components
-    p2 <- p2 +
-      geom_point(stat = "identity", size = 3, aes(col = col), position = position_dodge(width = 1)) +
-      geom_errorbar(aes(ymin = lwr.ci, ymax = upr.ci),
-                    width = .2, size = 0.5,
-                    position = "dodge"
-      ) +
-      xlab("") +
-      ylab("Difference between proportions (%)") +
-      geom_hline(yintercept = 0, lty = 2) +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(color = "black"),
-        axis.ticks.x = element_line(color = "black", linewidth = 0.5)
-      ) +
-      coord_flip() +
-      theme(legend.position = "none") +
-      theme(axis.text.y = element_blank())
-    p2 <- p2 + scale_color_manual(values = c("darkorange", "deepskyblue"))
-    
-    p1 + p2
-    
-    ###
-    ##
-    dir.create("output",recursive = T)
-    # height=5.5
-    ggsave(filename = glue::glue("output/", out.name, comparison,padj, "_metagenomics_smallSize.pdf"), width = 12, height = height)
-    ggsave(filename = glue::glue("output/", out.name, comparison,padj, "_metagenomics_mediumSize.pdf"), width = 12.5, height = height+2)
-    ggsave(filename = glue::glue("output/", out.name, comparison,padj, "_metagenomics_bigSize.pdf"), width = 12.5, height = height+3)
-    
-    #  }
   }
-  
 }
 
 
@@ -168,17 +168,18 @@ kegg.table.df <- cbind(name_id,selected_cols)
 identical(sort(colnames(kegg.table.df)[-1]), sort(rownames(metadata)))
 colnames(metadata)<-c("sampleID", "Group")
 
-pathway_pdf(meta_info=metadata,pathway_df=kegg.table.df,meta_col=c("sampleID", "Group"),out.name= glue::glue("Product_"),height = 5.5,padj=0.05)
+pathway_pdf(meta_info=metadata,pathway_df=kegg.table.df,meta_col=c("sampleID", "Group"),out.name= glue::glue("Product_"),height = 2,padj=0.05)
 
-pathway_pdf(meta_info=metadata,pathway_df=kegg.table.df,meta_col=c("sampleID", "Group"),out.name= glue::glue("Product_"),height = 5.5,padj=0.01)
+pathway_pdf(meta_info=metadata,pathway_df=kegg.table.df,meta_col=c("sampleID", "Group"),out.name= glue::glue("Product_"),height = 2,padj=0.01)
 
 
 
 # 
 # 
-# # 
+#
 # meta_info=metadata
 # pathway_df=kegg.table.df
 # meta_col=c("sampleID", "Group")
 # out.name="KEGG_"
-
+# height = 5.5
+# padj=0.01
